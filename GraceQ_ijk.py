d1be702424cc3900376c3c09ca6e9820d8778ee3
dhruvparekh008 <76069208+dhruvparekh008@users.noreply.github.com>
2022-04-20 01:57:44 +0530

    Add files via upload

---
+++ GraceQ_ijk.py
@@ -0,0 +1,32 @@
+import random
+import timeit
+
+
+num = 512
+
+
+p = [[0]*num]*num
+for i in range(num):
+    for j in range(num):
+        p[i][j] = random.randint(1,100)
+
+q = [[0]*num]*num
+for i in range(num):
+    for j in range(num):
+        q[i][j] = random.randint(1,100)
+
+r = [[0]*num]*num
+
+starttime = timeit.default_timer()
+for k in range(num):
+    for j in range(num):
+        for i in range(num):
+            r[i][j] = p[i][k]*q[k][j]
+stoptime = timeit.default_timer()
+
+for i in range(num):
+    print()
+    for j in range(num):
+        print(r[i][j] , end = " ")
+print()
+print('Total Time: ', stoptime - starttime)
